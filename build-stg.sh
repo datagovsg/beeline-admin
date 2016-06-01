88fd59917decf7cc99a8590e5efcc6f85ae994e1
daniel sim <daniel_sim@data.gov.sg>
2016-06-01 16:59:27 +0800

    delete staging deployment stuff

--- build-stg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export AUTH0_CID=BslsfnrdKMedsmr9GYkTv7ejJPReMgcE
-export AUTH0_DOMAIN=beeline.au.auth0.com
-export BACKEND_URL=https://beeline-server-dev.herokuapp.com
-export OUTPUT_FILENAME=bundle-staging.js
-
-webpack
-
