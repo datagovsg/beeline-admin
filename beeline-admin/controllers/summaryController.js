4cb3c314ad66a3e7e6b5fdbbd5cba49c0584f479
daniel sim <daniel_sim@data.gov.sg>
2016-08-02 16:02:57 +0800

    price/capacity summary

--- beeline-admin/controllers/summaryController.js
+++ beeline-admin/controllers/summaryController.js
@@ -44,6 +44,29 @@ export default function($scope, AdminService, RoutesService, LoadingSpinner) {
           trip.date = new Date(trip.date)
           route.tripsByDay[trip.date.getUTCDate() - 1] = trip
         }
+
+        let lastPrice, lastCapacity;
+        route.priceSummary = _.reduce(
+          route.tripsByDay,
+          (acc, value, index, coll) => {
+            if (acc.length == 0 ||
+                (value !== undefined &&
+                  ( acc[acc.length - 1].price !== value.price ||
+                    acc[acc.length - 1].capacity !== value.capacity))) {
+
+              acc.push({
+                price: value.price,
+                capacity: value.capacity,
+                count: 1
+              })
+            }
+            else {
+              acc[acc.length - 1].count++;
+            }
+            return acc;
+          },
+          [{price: undefined, capacity: undefined, count: 0}])
+          .filter(s => s.count)
       }
 
       $scope.routes = routes;
