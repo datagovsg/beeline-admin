fcb5639a8bbca19ddb9500266bf437afa55e8b71
shen_xujing <shen_xujing@ida.gov.sg>
2016-08-11 16:43:00 +0800

    1st commit

--- beeline-admin/directives/spanSelect.js
+++ beeline-admin/directives/spanSelect.js
@@ -2,7 +2,7 @@
 /*
   Extends the multiple-date-picker to support span select.
 */
-
+var offset = new Date().getTimezoneOffset() * 60000;
 export default function () {
   return {
     scope: {
@@ -12,10 +12,37 @@ export default function () {
       highlightDays: '=?',
     },
     template: `<multiple-date-picker
-      highlight-days="highlightDays"
+      highlight-days="combineHD"
       ng-model="ngModel">
     </multiple-date-picker>`,
     link(scope, elem, attr) {
+      scope.phs = [
+        {date:(moment('20160811')).valueOf(),css: 'holiday',title:'national holiday'},
+        {date:(moment('20160912')).valueOf(),css: 'holiday',title:'hari raya'}
+      ]
+      scope.$watch('highlightDays',(hds)=>{
+        for (var ph in scope.phs){
+          for (var hd in hds){
+            console.log(scope.phs[ph].date);
+            console.log(hds[hd].date);
+            if (moment(scope.phs[ph].date).isSame(hds[hd].date,'day')){
+            	console.log("same date");
+              _.assign(scope.phs[ph], hds[hd]);
+              console.log("merged as");
+              console.log(scope.phs[ph]);
+              hds.splice(hd,1);
+              break;
+            }
+          }
+        }
+        console.log('phs are ');
+        console.log(scope.phs);
+        console.log('hds are ');
+        console.log(hds);
+        scope.combineHD = _.concat(scope.phs, hds);
+        console.log('combine HDs are ');
+        console.log(scope.combineHD);
+      },true);
       scope.$watch('ngModel', (newValue, oldValue) => {
         console.log(newValue);
 
