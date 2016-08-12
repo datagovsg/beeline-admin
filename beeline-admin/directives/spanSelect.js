5b5a33dacfe48adaffd97fe0a9939ca88c548958
shen_xujing <shen_xujing@ida.gov.sg>
2016-08-12 10:28:04 +0800

    get holidays from server by calling /publicHolidays

--- beeline-admin/directives/spanSelect.js
+++ beeline-admin/directives/spanSelect.js
@@ -3,7 +3,7 @@
   Extends the multiple-date-picker to support span select.
 */
 var offset = new Date().getTimezoneOffset() * 60000;
-export default function () {
+export default function (AdminService) {
   return {
     scope: {
       firstDate: '=?',
@@ -16,36 +16,34 @@ export default function () {
       ng-model="ngModel">
     </multiple-date-picker>`,
     link(scope, elem, attr) {
-      scope.phs = [
-        {date:(moment('20160811')).valueOf(),css: 'holiday',title:'national holiday'},
-        {date:(moment('20160912')).valueOf(),css: 'holiday',title:'hari raya'}
-      ]
+      scope.phs = [];
+      var promise = AdminService.beeline({
+        method: 'GET',
+        url: `/publicHolidays`
+      })
+      .then((response) => {
+        var rs = response.data;
+        for (var index in rs) {
+          var ph = {date: rs[index].date, css: 'holiday', selectable: true, title: rs[index].summary};
+          scope.phs.push(ph);
+        }
+      });
+
       scope.$watch('highlightDays',(hds)=>{
-        for (var ph in scope.phs){
+        var phs = scope.phs;
+        for (var ph in phs){
           for (var hd in hds){
-            console.log(scope.phs[ph].date);
-            console.log(hds[hd].date);
-            if (moment(scope.phs[ph].date).isSame(hds[hd].date,'day')){
-            	console.log("same date");
-              _.assign(scope.phs[ph], hds[hd]);
-              console.log("merged as");
-              console.log(scope.phs[ph]);
+            if (moment(phs[ph].date).isSame(hds[hd].date,'day')){
+              _.assign(phs[ph], hds[hd]);
               hds.splice(hd,1);
               break;
             }
           }
         }
-        console.log('phs are ');
-        console.log(scope.phs);
-        console.log('hds are ');
-        console.log(hds);
-        scope.combineHD = _.concat(scope.phs, hds);
-        console.log('combine HDs are ');
-        console.log(scope.combineHD);
+        scope.combineHD = _.concat(phs, hds);
       },true);
       scope.$watch('ngModel', (newValue, oldValue) => {
         console.log(newValue);
-
 
         if (!newValue || newValue.length === 0) {
           if (scope.firstDate && scope.lastDate) {
