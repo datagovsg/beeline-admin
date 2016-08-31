543b780656078fd09d1bfaa30a460cb98ade36fb
daniel sim <daniel_sim@data.gov.sg>
2016-08-31 15:17:07 +0800

    set the filter on/off based on saved values

--- beeline-admin/directives/eventSubscriptionEditor/eventSubscriptionEditor.js
+++ beeline-admin/directives/eventSubscriptionEditor/eventSubscriptionEditor.js
@@ -9,12 +9,14 @@ export default function ($rootScope, $location, uiGmapGoogleMapApi, $q,
     },
     link (scope, elem, attr) {
       scope.options = {};
-      
+
+      scope.options.setTransportCompanyIds = _.get(scope, 'ngModel.transportCompanyIds', false) && true
+      scope.options.setRouteIds = _.get(scope, 'ngModel.routeIds', false) && true
+
       RoutesService.getCurrentRoutes()
       .then((routes) => {
         scope.routes = routes
       })
-
       scope.$watch('options.setTransportCompanyIds', (isSet) => {
         if (!isSet) {
           _.set(scope, 'ngModel.transportCompanyIds', undefined);
