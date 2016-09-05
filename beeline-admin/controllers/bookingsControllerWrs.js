6b76d609a71c81ae5898901dba8209527550a911
daniel sim <daniel_sim@data.gov.sg>
2016-09-05 10:32:45 +0800

    fix calendar not showing #bookings for last day

--- beeline-admin/controllers/bookingsControllerWrs.js
+++ beeline-admin/controllers/bookingsControllerWrs.js
@@ -228,7 +228,7 @@ export default function($scope, AdminService, RoutesService, LoadingSpinner,
 
   $scope.monthChanged = function (newMonth) {
     startOfMonth = newMonth.clone().startOf('month').toDate()
-    endOfMonth = newMonth.clone().endOf('month').startOf('day').toDate()
+    endOfMonth = newMonth.clone().startOf('month').add(1, 'month').add(-1, 'day').toDate()
     $scope.filter.startDate = startOfMonth;
     $scope.filter.endDate = endOfMonth;
   }
@@ -267,6 +267,7 @@ export default function($scope, AdminService, RoutesService, LoadingSpinner,
       console.log(err)
     });
 
+    // Update the annotations for the month
     var requestUrlOR = buildQuery({
       tripStartDate: Date.UTC(
         startOfMonth.getFullYear(),
@@ -276,7 +277,7 @@ export default function($scope, AdminService, RoutesService, LoadingSpinner,
       tripEndDate: Date.UTC(
         endOfMonth.getFullYear(),
         endOfMonth.getMonth(),
-        endOfMonth.getDate()
+        endOfMonth.getDate() + 1
       )
     });
 
