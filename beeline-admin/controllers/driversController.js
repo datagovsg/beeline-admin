6d7a9c314f8087ef11fe347d28ec9fbc7a443b4d
Yi Xin Ang <angyixin@gmail.com>
2016-09-23 11:20:43 +0800

    Driver remarks editing

--- beeline-admin/controllers/driversController.js
+++ beeline-admin/controllers/driversController.js
@@ -1,6 +1,6 @@
 import querystring from 'querystring'
 
-export default function ($scope, AdminService, LoadingSpinner) {
+export default function ($scope, AdminService, LoadingSpinner, commonModals) {
   $scope.drivers = [];
 
   function query() {
@@ -29,18 +29,24 @@ export default function ($scope, AdminService, LoadingSpinner) {
     });
   };
 
-  $scope.updateDriverName = (did) => {
-    var newName = prompt('Please enter the name of the Driver.');
+  $scope.updateDriverName = async (driver) => {
+    var newName = await commonModals.prompt({
+      message: 'Please enter the name of the Driver.',
+      default: driver.name
+    });
 
     if (!newName) return;
 
-    var newRemarks = prompt("Remarks?");
+    var newRemarks = await commonModals.prompt({
+      message: 'Remarks?',
+      default: driver.transportCompanies[0].driverCompany.remarks
+    });
 
     if (!newRemarks) return;
 
     LoadingSpinner.watchPromise(AdminService.beeline({
       method: 'PUT',
-      url: `/companies/${AdminService.getCompanyId()}/drivers/${did}`,
+      url: `/companies/${AdminService.getCompanyId()}/drivers/${driver.id}`,
       data: {
         name: newName,
         remarks: newRemarks
