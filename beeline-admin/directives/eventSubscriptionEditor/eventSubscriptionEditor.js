dcf5a70a7a91bd97bc04001011643f868de432b0
daniel sim <daniel_sim@data.gov.sg>
2016-10-05 10:19:13 +0800

    fix time dropdown, add additional options, clean up UI

--- beeline-admin/directives/eventSubscriptionEditor/eventSubscriptionEditor.js
+++ beeline-admin/directives/eventSubscriptionEditor/eventSubscriptionEditor.js
@@ -9,14 +9,22 @@ export default function ($rootScope, $location, uiGmapGoogleMapApi, $q,
     },
     link (scope, elem, attr) {
       scope.options = {};
+      scope.disp = {};
 
-      scope.options.setTransportCompanyIds = _.get(scope, 'ngModel.transportCompanyIds', false) && true
-      scope.options.setRouteIds = _.get(scope, 'ngModel.routeIds', false) && true
+      scope.disp.timeOptions = [
+        [60000 * 5, '5 minutes before trip'],
+        [60000 * 15, '15 minutes before trip'],
+        [60000 * 25, '25 minutes before trip'],
+        [60000 * 30, '30 minutes before trip'],
+      ]
 
       RoutesService.getCurrentRoutes()
       .then((routes) => {
         scope.routes = routes
       })
+
+      scope.options.setTransportCompanyIds = _.get(scope, 'ngModel.transportCompanyIds', false) && true
+      scope.options.setRouteIds = _.get(scope, 'ngModel.routeIds', false) && true
       scope.$watch('options.setTransportCompanyIds', (isSet) => {
         if (!isSet) {
           _.set(scope, 'ngModel.transportCompanyIds', undefined);
@@ -27,7 +35,6 @@ export default function ($rootScope, $location, uiGmapGoogleMapApi, $q,
           _.set(scope, 'ngModel.routeIds', undefined);
         }
       });
-
       scope.$watch(() => AdminService.getCompanyId(), (cid) => {
         _.set(scope, 'ngModel.transportCompanyIds', [cid]);
       })
