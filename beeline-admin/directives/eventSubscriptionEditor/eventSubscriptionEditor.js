ae176b6843dfee6dd2d23bc50d8bef365490f586
daniel_sim <daniel_sim@data.gov.sg>
2016-10-07 18:09:54 +0800

    more event types

--- beeline-admin/directives/eventSubscriptionEditor/eventSubscriptionEditor.js
+++ beeline-admin/directives/eventSubscriptionEditor/eventSubscriptionEditor.js
@@ -13,13 +13,24 @@ export default function ($rootScope, $location, uiGmapGoogleMapApi, $q,
       scope.options = {};
       scope.disp = {};
 
-      scope.disp.timeOptions = [
+      scope.disp.noPingTimeOptions = [
+        [5, '5 minutes before trip'],
+        [15, '15 minutes before trip'],
+        [25, '25 minutes before trip'],
+      ]
+
+      scope.disp.urgentBookingTimeOptions = [
         [60000 * 5, '5 minutes before trip'],
+        [60000 * 10, '10 minutes before trip'],
         [60000 * 15, '15 minutes before trip'],
-        [60000 * 25, '25 minutes before trip'],
         [60000 * 30, '30 minutes before trip'],
       ]
 
+      scope.disp.lateArrivalTimeOptions = [
+        [60000 * 5, 'more than 5 mins late'],
+        [60000 * 10, 'more than 10 mins late'],
+        [60000 * 15, 'more than 15 mins late'],
+      ]
 
       scope.$watch(() => AdminService.getCompanyId(), (cid) => {
         RoutesService.getCurrentRoutes()
