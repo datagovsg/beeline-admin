0a626c6d339aaeedf3eff1a491c76296ca68443d
daniel <daniel_sim@data.gov.sg>
2016-11-01 19:19:34 +0800

    user friendly notification management

--- beeline-admin/controllers/notificationsController.js
+++ beeline-admin/controllers/notificationsController.js
@@ -7,13 +7,13 @@ export default function ($scope, AdminService, LoadingSpinner, commonModals, com
   ////// Data declarations
   $scope.disp = {
     eventTypes: [
-      ['noPings', 'Driver App not switched on'],
-      ['tripCancelled', 'Trip cancelled by driver'],
-      ['passengersMessaged', 'Message broadcast to passengers'],
-      ['newBooking', 'New booking made'],
-      ['urgentBooking', 'Last minute booking made'],
-      ['lateETA', 'Bus predicted to arrive late'],
-      ['lateArrival', 'Bus arrives late at destination'],
+      // ['noPings', 'Driver App not switched on'],
+      // ['tripCancelled', 'Trip cancelled by driver'],
+      // ['passengersMessaged', 'Message broadcast to passengers'],
+      // ['newBooking', 'New booking made'],
+      // ['urgentBooking', 'Last minute booking made'],
+      // ['lateETA', 'Bus predicted to arrive late'],
+      // ['lateArrival', 'Bus arrives late at destination'],
     ].concat( AdminService.isSuperAdmin() ? [
       ['lifecycle', 'Server started'],
       ['transactionFailure', 'Payment problems']
@@ -30,6 +30,7 @@ export default function ($scope, AdminService, LoadingSpinner, commonModals, com
     }))
     .then((response) => {
       $scope.eventSubscriptions = _.sortBy(response.data, r => r.agent && r.agent.name);
+      $scope.subscriptions = RouteNotifications.parse($scope.eventSubscriptions);
     })
   }
 
@@ -43,7 +44,70 @@ export default function ($scope, AdminService, LoadingSpinner, commonModals, com
     'params', 'event', 'handler', 'formatter', 'agent'
   ]
 
-  $scope.subscriptions = {
+  $scope.routeSubscriptions = {
+    add() {
+      $scope.subscriptions.push({
+        agent: {},
+        events: {},
+        handler: '',
+        options: {},
+        ids: []
+      })
+    },
+    saveOne(subscr) {
+      console.log('save one?')
+      var newEntries = RouteNotifications.serialize(subscr);
+
+      // insert the new entries, then delete the old ones
+      LoadingSpinner.watchPromise(Promise.all(newEntries.map(ne =>
+        AdminService.beeline({
+          method: 'POST',
+          url: `/companies/${companyId}/eventSubscriptions`,
+          data: ne
+        })
+      ))
+      .then((responses) => {
+        _.zip(newEntries, responses).forEach(([entry, response]) => {
+          entry.id = response.data.id;
+        })
+
+        console.log('test?')
+
+        $scope.subscriptions.splice(
+          $scope.subscriptions.indexOf(subscr), 1,
+          RouteNotifications.parse(responses.map(r => r.data))[0]
+        )
+
+        console.log(_.uniqBy(subscr.ids))
+
+        return Promise.all(_.uniqBy(subscr.ids).map(id =>
+          AdminService.beeline({
+            method: 'DELETE',
+            url: `/companies/${companyId}/eventSubscriptions/${id}`
+          })))
+          .then(() => $scope.$digest())
+      }))
+    },
+    deleteOne(subscr) {
+      if (subscr.ids) {
+        LoadingSpinner.watchPromise(Promise.all(_.uniqBy(subscr.ids).map(id =>
+          AdminService.beeline({
+            method: 'DELETE',
+            url: `/companies/${companyId}/eventSubscriptions/${id}`
+          }))))
+        .then(() => {
+          $scope.subscriptions.splice($scope.subscriptions.indexOf(subscr), 1)
+        })
+        .then(() => $scope.$digest())
+        .catch(err => commonModals.alert(err.data.message))
+      }
+      else {
+        $scope.subscriptions.splice($scope.subscriptions.indexOf(subscr), 1)
+      }
+    }
+  }
+
+  $scope.subscriptionMethods = {
     add() {
       $scope.eventSubscriptions.push(defaultEventSubscription())
     },
@@ -89,5 +153,141 @@ export default function ($scope, AdminService, LoadingSpinner, commonModals, com
         $scope.eventSubscriptions.splice($scope.eventSubscriptions.indexOf(subscr), 1)
       }
     }
+  }
+}
+
+const events = {
+  newBooking: {
+    event: 'newBooking',
+  },
+  noPings5: {
+    event: 'noPings',
+    defaultParams: {
+      minsBefore: [5],
+    }
+  },
+  noPings15: {
+    event: 'noPings',
+    defaultParams: {
+      minsBefore: [15],
+    }
+  },
+  noPings25: {
+    event: 'noPings',
+    defaultParams: {
+      minsBefore: [25],
+    }
+  },
+  lateArrival: {
+    event: 'lateArrival',
+    defaultParams: {
+      timeAfter: [10*60000],
+    }
+  },
+  lateETA: {
+    event: 'lateETA',
+    defaultParams: {
+      timeAfter: [10*60000],
+    }
+  },
+  passengersMessaged: {
+    event: 'passengersMessaged',
+  },
+  tripCancelled: {
+    event: 'tripCancelled',
+  },
+}
+
+const RouteNotifications = {
+  // Convert from human-unreadable database entries
+  // to a human-summarizable entries
+  parse(eventSubscriptions) {
+    var relevantKeys = _(events).values().map(e => e.event).keyBy().value();
+
+    var groupedByRoutes = _(eventSubscriptions)
+      .filter(e => e.event in relevantKeys)
+      .groupBy(e => {
+        return _.sortBy(e.params.routeIds || []).join(',') + '|' +
+          e.params.ignoreIfEmpty + '|' +
+          e.handler + '|' +
+          JSON.stringify(e.agent) // FIXME: this is not guaranteed to work reliably?
+      })
+      .mapValues((es, g) => {
+        const ev = _(events).keys().map((key) => {
+          function satisfiesEvent(e, eventConditions) {
+            // Ensure that array keys are superset of the same
+            // key in e
+
+            return e.event == eventConditions.event &&
+              _.keys(eventConditions.defaultParams)
+              .filter(k => eventConditions.defaultParams[k] instanceof Array)
+              .every(k => _.intersection(eventConditions.defaultParams[k], e.params[k]).length
+                == eventConditions.defaultParams[k].length)
+          }
+
+          // Check if this key applies to some of the event subscriptions
+          return [key,
+            es.find(e => satisfiesEvent(e, events[key]))
+          ]
+        }).fromPairs().value()
+
+        return {
+          options: _(es[0].params)
+            .pick(['routeIds', 'ignoreIfEmpty'])
+            .filter(v => v !== undefined)
+            .value(), /* no undefined route ids */
+          handler: es[0].handler,
+          agent: es[0].agent,
+          events: _.mapValues(ev, e => !!e),
+          ids: _(ev).mapValues(e => e && e.id).filter(x => x).value(),
+        }
+      })
+      .values()
+      .value();
+
+    console.log(groupedByRoutes)
+
+    return groupedByRoutes
+  },
+
+  serialize(subscription) {
+    var eventSubscriptions = _.keys(subscription.events)
+      .filter(key => subscription.events[key])
+      .map(key => ({
+        event: events[key].event,
+        formatter: '0',
+        params: _.defaults(events[key].defaultParams,
+            subscription.options),
+        agent: subscription.agent,
+        handler: subscription.handler,
+        id: subscription.ids && subscription.ids[key]
+      }))
+    var mergedSubscriptions = _(eventSubscriptions)
+      .groupBy('event')
+       // merge the array-valued params
+      .mapValues(vs => {
+        return _.reduce(vs, (acc, v) => {
+          if (acc == null) {
+            return v;
+          } else {
+            // merge array-valued params
+            var params = _.defaults(acc.params, v.params);
+
+            for (let key in acc.params) {
+              if (acc.params[key] instanceof Array &&
+                    v.params[key] instanceof Array) {
+                acc.params[key] = acc.params[key].concat(v.params[key])
+              }
+            }
+            acc.params = params
+
+            return acc;
+          }
+        }, null);
+      })
+      .values()
+      .value()
+
+      return mergedSubscriptions;
   }
 }
