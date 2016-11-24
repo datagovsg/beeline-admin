91f55c7f45af3bfb366757e4533744a692fa8ac7
daniel sim <daniel_sim@data.gov.sg>
2016-11-24 16:22:52 +0800

    Use ui-select for stop selector

--- beeline-admin/directives/selectors/stop.js
+++ beeline-admin/directives/selectors/stop.js
@@ -2,21 +2,53 @@
 export default function(RoutesService) {
   return {
     template: `
-<select
-    ng-options="stop.id as stop.description for stop in stops | orderBy:'description'"
-    >
-</select>
+<ui-select ng-model="data.stop" on-select="updateModel()">
+  <ui-select-match placeholder="Select a bus stop...">
+    {{$select.selected.description}}
+  </ui-select-match>
+  <ui-select-choices repeat="stop in stops"
+    refresh="updateSearch($select.search)"
+    refresh-delay="150">
+    <div>{{stop.description}}</div>
+  </ui-select-choices>
+</ui-select>
     `,
-    replace: true,
-    scope: true,
+    scope: {
+      ngModel: '=',
+    },
     link(scope, elem, attr) {
+      var allStops = [];
+
       scope.stops = [];
+      scope.data = {
+        stop: {
+          id: scope.ngModel,
+          description: 'Loading...'
+        }
+      };
 
       RoutesService.getStops()
 
+      ////////////////////////////////////////////
+      // Methods
+      scope.updateModel = function() {
+        console.log(scope.ngModel, scope.data.stop)
+        console.log(scope.ngModel, scope.data.stop.id)
+        scope.ngModel = scope.data.stop.id;
+      }
+
+      scope.updateSearch = function (search) {
+        scope.stops = allStops.filter(s =>
+          s.description.toUpperCase().indexOf(search.toUpperCase()) !== -1)
+      }
+
+      // Load the data
       scope.$watch(() => RoutesService.stopsPromise, () => {
         RoutesService.stopsPromise.then((stops) => {
-          scope.stops = stops
+          scope.stops = allStops = _.sortBy(stops, 'description')
+          if (scope.data.stop) {
+            scope.data.stop = allStops.find(s => s.id === scope.data.stop.id)
+          }
         })
       })
     },
