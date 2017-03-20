d0df5f99e9205b1b8c28c10daf24b3f7f040f4fe
Daniel Sim <daniel.ssq89@gmail.com>
2017-03-20 16:48:51 +0800

    Divert admin requests to https://beeline-monitoring-api.herokuapp.com/

--- build-wrs.sh
+++ build-wrs.sh
@@ -2,7 +2,7 @@
 
 export AUTH0_CID=BslsfnrdKMedsmr9GYkTv7ejJPReMgcE
 export AUTH0_DOMAIN=beeline.au.auth0.com
-export BACKEND_URL=https://api.beeline.sg
+export BACKEND_URL=https://beeline-monitoring-api.herokuapp.com/
 
 # webpack
 gulp
