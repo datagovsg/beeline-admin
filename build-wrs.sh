73b7416962f13de82ae96f20acb905a5fd6ea878
Daniel Sim <daniel.ssq89@gmail.com>
2017-03-20 16:58:42 +0800

    Update build-wrs.sh

--- build-wrs.sh
+++ build-wrs.sh
@@ -2,7 +2,7 @@
 
 export AUTH0_CID=BslsfnrdKMedsmr9GYkTv7ejJPReMgcE
 export AUTH0_DOMAIN=beeline.au.auth0.com
-export BACKEND_URL=https://beeline-monitoring-api.herokuapp.com
+export BACKEND_URL=https://beeline-server-admin.herokuapp.com
 
 # webpack
 gulp
