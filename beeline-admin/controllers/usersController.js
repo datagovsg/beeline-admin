8826b3c0bec4d4e4130b3b5d389a3a0aa66f4116
Ray <chengwm@gmail.com>
2017-03-24 14:58:06 +0800

    Add functionality to retrieve route credits for user

--- beeline-admin/controllers/usersController.js
+++ beeline-admin/controllers/usersController.js
@@ -5,26 +5,33 @@ angular.module('beeline-admin')
     LoadingSpinner, $state, $stateParams, issueTicketModal, commonModals, $uibModal) {
 
   $scope.user = null
-  $scope.selector = { userId: null }
+  $scope.selector = { userId: $stateParams.userId || null }
   $scope.routeCredits = null
   $scope.adminService = AdminService
+  $scope.companyId = $stateParams.companyId || null
 
   $scope.$watch('selector.userId', userId => {
-    LoadingSpinner.watchPromise(
-      AdminService.beeline({
-        method: 'GET',
-        url: `/user/${userId}`
+    if(userId){
+      LoadingSpinner.watchPromise(
+        AdminService.beeline({
+          method: 'GET',
+          url: `/user/${userId}`
+        })
+      ).then(resp => {
+        $scope.user = resp.data;
+      }).catch(err => {
+        console.log(err)
+        // popup of error, then redirect back to main page
       })
-    ).then(resp => {
-      $scope.user = resp.data;
-    }).catch(err => {
-      console.log(err)
-      // popup of error, then redirect back to main page
-    })
-  })
 
-  $scope.selector.userId = $stateParams.userId 
-  $scope.companyId = $stateParams.companyId
+      if($scope.companyId){
+        RoutesService.fetchRouteCredits(userId, $scope.companyId)
+        .then(routeCredits => {
+          $scope.routeCredits = routeCredits
+        })
+      }
+    }
+  })
 
   $scope.getUserPin = async function (){
     const userId = $scope.user.id
