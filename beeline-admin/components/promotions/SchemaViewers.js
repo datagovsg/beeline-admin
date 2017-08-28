cdeb0ace8e2582b301dafd87447ec342907def0a
daniel sim <daniel_sim@data.gov.sg>
2017-08-28 16:51:33 +0800

    promotions UI update -- display with schema viewers

--- beeline-admin/components/promotions/SchemaViewers.js
+++ beeline-admin/components/promotions/SchemaViewers.js
@@ -1,4 +1,5 @@
 import dateformat from 'dateformat'
+import {mapActions, mapState, mapGetters} from 'vuex'
 
 export const DateFormatViewer = {
   props: {
@@ -8,16 +9,85 @@ export const DateFormatViewer = {
   },
   render (h) {
     // FIXME: why doesn't Vue expose the text node constructor?
-    return this._v(dateformat(this.value, this.format, this.utc))
+    return this._v(this.value ? dateformat(this.value, this.format, this.utc) : '')
   }
 }
 
 export const ContactListViewer = { // Stub
   props: ['value'],
   render (h) {
-    // FIXME: why doesn't Vue expose the text node constructor?
-    return this._v(JSON.stringify(this.value))
+    const matchingContactList = (this.contactLists && this.contactLists.find(c => c.id === this.value))
+    return h(
+      'a',
+      {domProps: {href: `#/c/${this.companyId}/contactLists/${this.value}`}},
+      [
+        matchingContactList ? matchingContactList.description : this.value
+      ]
+    )
+  },
+  created () {
+    this.fetch(['contactLists'])
+  },
+  computed: {
+    ...mapState('companyShared', ['contactLists', 'companyId'])
+  },
+  methods: {
+    ...mapActions('companyShared', ['fetch'])
   }
 }
 
-export const RouteIdsViewer = ContactListViewer // Stub
+export const CompanyIdViewer = { // Stub
+  props: ['value'],
+  render (h) {
+    return h(
+      'a',
+      {domProps: {href: `#/c/${this.value}/companies`}},
+      [(this.companiesById && this.companiesById[this.value])
+        ? this.companiesById[this.value].name
+        : `#${this.value}`]
+    )
+  },
+  created () {
+    this.fetch(['companies'])
+  },
+  computed: {
+    ...mapGetters('shared', ['companiesById']),
+    ...mapState('companyShared', ['companyId']),
+  },
+  methods: {
+    ...mapActions('shared', ['fetch'])
+  }
+}
+
+export const RouteIdsViewer = {
+  props: ['value'],
+  render (h) {
+    const contents = []
+
+    for (let v of this.value) {
+      const route = this.allRoutesById[v]
+      if (contents.length !== 0) {
+        contents.push(', ')
+      }
+      contents.push(h(
+        'a',
+        {domProps: {href: `#/c/${route && route.transportCompanyId}/trips/${v}/route`}},
+        [(route && route.label) || `#${v}`]
+      ))
+    }
+
+    return h(
+      'span',
+      contents
+    )
+  },
+  created () {
+    this.fetch(['allRoutes'])
+  },
+  computed: {
+    ...mapGetters('shared', ['allRoutesById'])
+  },
+  methods: {
+    ...mapActions('shared', ['fetch'])
+  }
+}
