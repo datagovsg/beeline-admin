c4b200823d8ae04ff2eabd6de40319940cd10faa
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-05-22 16:10:13 +0800

    Flesh out timeliness reporting functionality
    * Bring in downloadjs as a dependency
    * Fill out all the needed styling and logic for a barebones timeliness reporting tool
    
    TODO: Multi-route download, handling case when there are no trips running on a given day

--- beeline-admin/pages/route-timeliness.vue
+++ beeline-admin/pages/route-timeliness.vue
@@ -1,7 +1,179 @@
 <template>
+  <div class="container-fluid withnav route-passes">
+    <LoadingSpinner ref="loadingSpinner"/>
+    <ModalHelper ref="modalHelper"/>
+
+    <div class="row" v-if="!companyId">
+      Please select a company
+    </div>
+    <div class="row">
+      <div class="col-sm-8">
+        <h1>Route Timeliness</h1>
+        <form>
+          <div class="form-group">
+            <label>Route</label>
+            <route-selector
+              class="form-control"
+              v-model="filter.routeId"
+              :companyId="companyId"
+              :multiple="false"
+              />
+          </div>
+          <br>
+
+        </form>
+        <button class="btn btn-default" @click="downloadCSV()" type="button">
+          <span class="glyphicon glyphicon-save" aria-hidden="true"/>
+          Download CSV
+        </button>
+      </div>
+      <div class="col-sm-4">
+        <div class="datepicker-wrap">
+          <h4 class="text-center">
+            Dates selected:
+            {{ this.query.from }} - {{ this.query.to }}
+          </h4>
+          <span-select @month-changed="monthChanged" v-model="filter.dates" :special-dates="publicHolidayDates"/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import querystring from 'querystring'
+import {mapGetters, mapActions, mapState} from 'vuex'
+import _ from 'lodash'
+import download from 'downloadjs'
+import * as resources from '../stores/resources'
+import filters from '../filters'
+
+import RouteSelector from '../components/RouteSelector.vue'
+
 export default {
   props: ['companyId'],
+  data () {
+    return {
+      filter: {
+        dates: [],
+        selectedMonth: new Date(),
+        routeId: null,
+      },
+      publicHolidaysPromise: this.fetch('publicHolidays')
+    }
+  },
+  components: { RouteSelector },
+  computed: {
+    ...mapGetters(['axios']),
+    ...mapState('shared', ['publicHolidays']),
+
+    query () {
+      const { selectedMonth, dates, routeId } = this.filter
+
+      const from = filters.date(
+        dates.length > 0
+          ? dates[0]
+          : new Date(
+            selectedMonth.getFullYear(),
+            selectedMonth.getMonth(),
+            1
+          ),
+        'isoDate'
+      )
+
+      const to = filters.date(
+        dates.length > 1
+          ? dates[1]
+          : new Date(
+            selectedMonth.getFullYear(),
+            selectedMonth.getMonth() + 1,
+            0
+          ),
+        'isoDate'
+      )
+
+      console.log({ routeId, from, to })
+      return { routeId, from, to }
+    }
+  },
+  asyncComputed: {
+    publicHolidayDates: {
+      async get () {
+        await this.publicHolidaysPromise
+        return this.publicHolidays.map(ph => (
+          {
+            date: new Date(ph.date),
+            classes: ['public-holiday'],
+          }
+        ))
+      },
+      default: []
+    }
+  },
+
+  methods: {
+    ...mapActions('spinner', ['spinOnPromise']),
+    ...mapActions('modals', ['showModal', 'showErrorModal']),
+    ...mapActions('shared', ['fetch']),
+
+    downloadCSV() {
+      const { routeId, ...queryParameters } = this.query
+      const qs = querystring.stringify({ ...queryParameters, format: 'csv' })
+      this.axios
+        .get(`${process.env.TRACKING_URL}/routes/${routeId}/performance?${qs}`)
+        .then(response => {
+          const contentType = response.headers['content-type']
+          const blob = new Blob([response.data], { type: contentType })
+          const { from, to } = queryParameters
+          const fileName = `${routeId} - ${from} to ${to}.csv`
+          download(blob, fileName, contentType)
+        })
+    },
+    monthChanged (newMonth) {
+      this.filter.selectedMonth = newMonth.clone().toDate()
+      this.filter.startDate = this.filter.endDate = null
+    },
+  },
 }
 </script>
+
+<style lang="scss">
+.span-select {
+  width: 100%;
+  td, th {
+    text-align: center;
+    line-height: 3.0;
+    position: relative;
+
+    &.selected {
+      background-color: #008;
+      color: #FFF;
+    }
+    &.disabled {
+      background-color: #888;
+      color: #CCC;
+    }
+    &.different-month {
+      opacity: 0.5;
+    }
+    &:not(.different-month) {
+      font-weight: bold;
+    }
+    &.public-holiday {
+      color: #F00;
+    }
+    div.annotation {
+      background-color: #FF6C6A;
+      color: #F4F4F4;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      line-height: 1.6;
+      font-size: 12px;
+      padding: 1px 5px;
+    }
+  }
+  th:not([colspan]) {
+    width: 14%;
+  }
+}
+</style>
