f0d143cb0550b0bf0cfc9a65a03d3337e7bc4c18
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-06-19 10:34:22 +0800

    use jasmine assertions, and drop chai

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
     browsers: ['PhantomJS'],
-    frameworks: ['jasmine', 'sinon-chai', 'phantomjs-shim'],
+    frameworks: ['jasmine', 'phantomjs-shim'], // 'sinon-chai',
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
