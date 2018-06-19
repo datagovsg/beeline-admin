337e26612c959e9d56a6ce4ea9fc6b125ab2a052
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-06-19 11:30:18 +0800

    remove unnecessary controllers

--- beeline-admin/controllers/driversController.js
+++
@@ -1,9 +0,0 @@
-angular.module('beeline-admin')
-  .controller(
-    'driversController',
-    function($scope, $stateParams) {
-      $scope.pageProps = {
-        companyId: parseInt($stateParams.companyId),
-      };
-    }
-  );
