86a6be124c780f206150a306c67901ebf96f5513
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-06-19 10:34:46 +0800

    add adminpage spec

---
+++ test/unit/specs/AdminPage.spec.js
@@ -0,0 +1,136 @@
+import AdminPage from '@/pages/admins.vue'
+import { mount } from '@vue/test-utils'
+import { delay, mockAjax, testStore } from '../util'
+import axios from 'axios'
+import sinon from 'sinon'
+
+describe('admins.vue', () => {
+  let adminPage = null
+
+  beforeEach(async () => {
+    adminPage = await mockAjax({
+      'GET /companies/5/admins': [
+        200,
+        [
+          {
+            id: 101,
+            name: 'Amanda',
+            email: 'amanda@example.com',
+            transportCompanies: [
+              {
+                id: 5,
+                adminCompany: {
+                  permissions: []
+                }
+              }
+            ]
+          },
+          {
+            id: 103,
+            name: 'Aisha',
+            email: 'aisha@example.com',
+            transportCompanies: [
+              {
+                id: 5,
+                adminCompany: {
+                  permissions: []
+                }
+              }
+            ]
+          },
+          {
+            id: 102,
+            name: 'Aardvark',
+            email: 'aardvark@example.com',
+            transportCompanies: [
+              {
+                id: 5,
+                adminCompany: {
+                  permissions: []
+                }
+              }
+            ]
+          }
+        ]
+      ]
+    }, async () => {
+      const adminPage = mount(
+        AdminPage,
+        {
+          propsData: {companyId: 5},
+          store: testStore({})
+        }
+      )
+      console.log('JUST CREATED THE PAGE AGAIN!!')
+      await delay(10)
+      return adminPage
+    })
+  })
+
+  it('should render all the admins', async () => {
+    const rows = adminPage.findAll(`table.admin-table tbody tr`)
+
+    expect(rows.length).toBe(3)
+
+    // Note: expect the entries to be sorted by ID, so Aisha comes after Aardvark
+    expect(rows.at(0).text().includes('Amanda'))
+    expect(rows.at(1).text().includes('Aardvark'))
+    expect(rows.at(2).text().includes('Aisha'))
+  })
+
+  it('should delete admins on request', async () => {
+    let deleted = false
+
+    await mockAjax({
+      'DELETE /companies/5/admins/102': [200, {}, () => { deleted = true }]
+    }, async () => {
+      adminPage.findAll(`table.admin-table tbody tr`).at(1)
+        .find('.delete-button')
+        .trigger('click')
+
+      await delay(10)
+      expect(adminPage.find('.modal-footer .btn-primary').isVisible()).toBe(true)
+
+      expect(deleted).toBe(false)
+      adminPage.find('.modal-footer .btn-primary').trigger('click')
+      await delay(10)
+      expect(deleted).toBe(true)
+    })
+  })
+
+  it('should create admins on request', async () => {
+    let called = false
+
+    await mockAjax({
+      'POST /companies/5/admins': [
+        200,
+        {},
+        ({data}, response) => {
+          called = true
+          expect(data.name).toBe('Ben')
+          expect(data.email).toBe('benben@example.com')
+        }]
+    }, async () => {
+      adminPage.find('.add-admin')
+        .trigger('click')
+
+      // Extra row created
+      await delay(10)
+      expect(adminPage.findAll('table.admin-table tbody tr').length).toBe(4)
+
+      adminPage.find('table.admin-table tbody tr:nth-child(4) input[type="email"]')
+        .setValue('benben@example.com')
+
+      adminPage.find('table.admin-table tbody tr:nth-child(4) input[type="text"]')
+        .setValue('Ben')
+
+      adminPage.find('table.admin-table tbody tr:nth-child(4) .update-button')
+        .trigger('click')
+
+      await delay(1)
+      expect(called).toBe(true)
+    })
+  })
+
+  // PUT of admins is not allowed -- you have to delete and add again
+})
