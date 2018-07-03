afb338245d31fc735ffa236872f402a874c15b6a
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-07-03 17:14:56 +0800

    migrate to genericized vue pages

--- beeline-admin/router.js
+++ beeline-admin/router.js
@@ -170,10 +170,11 @@ angular.module('beeline-admin')
 
   .state('c.contact-list', {
     url: '/contactLists/{contactListId}',
-    templateUrl: 'templates/contact-list.html',
+    templateUrl: 'templates/generic-vue-page.html',
     controller: 'genericVuePageController',
     data: {
       requiresLogin: true,
+      page: 'ContactListPage',
     }
   })
 
