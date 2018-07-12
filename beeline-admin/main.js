7b1c231e8a891c0f79e179eccb400a2263c0ef60
daniel sim <daniel_sim@data.gov.sg>
2018-07-12 18:40:42 +0800

    migrate index.html -> App.vue

--- beeline-admin/main.js
+++ beeline-admin/main.js
@@ -1,187 +1,31 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import AsyncComputed from 'vue-async-computed'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import assert from 'assert'
 
-angular.module('beeline-admin', [
-  'uiGmapgoogle-maps', 'ui.router', 'ui.bootstrap',
-  'angular-storage', 'angular-jwt', 'ngCookies', 'multipleDatePicker',
-  'ui.select', 'ngTagEditor'])
+import App from './App.vue'
+import createViewStore from '@/stores'
+import createRouter from '@/router'
 
-global.moment = require('moment')
-
-require('angular-storage')
-require('angular-cookies')
-require('angular-jwt')
-require('multiple-date-picker')
-require('ui-select/dist/select')
-require('../ngTagEditor/ngTagEditor')
-require('./controllers/genericVuePageController.js')
-require('./services/uibModalPromise')
-require('./stores/vueStore')
-require('./directives/vue-page')
-require('./directives/percentInput')
-require('./directives/selectors/contactList')
-require('./directives/selectors/route2')
-require('./directives/routePassHistoryViewer/routePassHistoryViewer')
-require('./auth0')
-require('./router')
-
-angular.module('beeline-admin')
-.config(configureGoogleMaps)
-.config(configureUrlWhitelist)
-.directive('adminNav', require('./directives/adminNav/adminNav').default)
-.directive('accountView', require('./directives/accountView/accountView').default)
-.directive('paymentView', require('./directives/paymentView/paymentView').default)
-.directive('ticketView', require('./directives/ticketView/ticketView').default)
-.directive('tripInfoBroker', require('./directives/tripInfoBroker').default)
-.directive('companyInfoBroker', require('./directives/companyInfoBroker').default)
-.directive('tripSelectionBroker', require('./directives/tripSelector/tripSelectionBroker').default)
-.directive('multiSelectBroker', require('./directives/multiSelectBroker').default)
-.directive('companyLogo', require('./directives/companyLogo').default)
-.directive('mySort', require('./directives/mySort').default)
-.directive('spanSelect', require('./directives/spanSelect').default)
-.directive('pingPath', require('./directives/pingPath').default)
-/* Selectors */
-.directive('tripStatusSelector', require('./directives/selectors/tripStatus').default)
-.directive('stopSelector', require('./directives/selectors/stop').default)
-.directive('stopSelectorPopup', require('./directives/selectors/stopPopup').default)
-.directive('userSelector', require('./directives/selectors/user').default)
-.directive('superAdminCompanySelector', require('./directives/selectors/superAdminCompany').default)
-.directive('companySelector', require('./directives/selectors/company').default)
-.directive('expandableArea', require('./directives/expandableArea').default)
-.directive('phDatePicker', require('./directives/phDatePicker').default)
-.service('AdminService', require('./services/adminService').default)
-.service('TripsService', require('./services/tripsService').default)
-.service('RoutesService', require('./services/routesService').default)
-.service('StopsPopup', require('./services/stopsPopup').default)
-.service('RoutePopup', require('./services/routePopup').default)
-.service('mapService', require('./services/mapService').default)
-.service('DriverService', require('./services/driverService').default)
-.service('LoadingSpinner', require('./services/loadingSpinner').default)
-.service('commonModals', require('./services/commonModals').default)
-.service('TagsService', require('./services/tagsService').default)
-.filter('makeRoutePath', require('./shared/filters.js').makeRoutePath)
-.filter('intervalToTime', require('./shared/filters.js').intervalToTime)
-.filter('leftPad', () => require('left-pad'))
-.run(function ($rootScope, auth, store, $cookies, AdminService, jwtHelper, $state,
-               commonModals) {
-  let initialized = false
-
-  $rootScope.$on('$stateChangeStart', function($event, newState, newParams, oldState, oldParams) {
-    // We pause the state change when
-    // 1. Auth is not yet initialized. Initialization comprises two steps:
-    //    a) Fetching the domain & CID
-    //    b) Parsing the hash for the login token
-    //
-    //    .run() functions are run immediately on page load. We
-    //    need to pause any state change until (a) is complete, so that
-    //    we can decide whether the user is visitng the page for the first time,
-    //    or if the user has just been redirected back.
-    //
-    // 2. Refresh token needs to be used.
-    pauseStateChange($event, newState, newParams,
-      initialized
-        ? checkStorageToken()
-        : handleRedirect().then(checkStorageToken)
-    )
-  });
-
-  Promise.resolve(checkStorageToken()).catch(() => {})
-  handleRedirect()
-
-  function storeSessionToken({ idToken }) {
-    store.set('sessionToken', idToken)
-    auth.getProfile().then((profile) => {
-      store.set('profile', profile);
-    })
-  }
-
-  // If promise is a Promise, pause the state change until it's resolved
-  // else change the state immediately
-  function pauseStateChange($event, newState, newParams, promise) {
-    if (promise) {
-      $event.preventDefault();
-
-      promise.then(() => {
-        $state.go(newState.name, newParams)
-      })
-      .catch((preventLogin) => {
-        if (!preventLogin) {
-          auth.showLoginDialog()
-            .then(storeSessionToken)
-            .finally(() => window.location.reload())
-        }
-      })
-    } else {
-      return Promise.resolve(null)
+document.addEventListener('DOMContentLoaded', () => {
+  Vue.use(Vuex)
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyBkFH42PlbFrsfdAnjw37qMLAxjhkMT-54',
+      libraries: 'geometry',
     }
-  }
-
-  // Handle when the user is redirected back to the page
-  function handleRedirect () {
-    return auth.domainPromise.then(({authResult}) => {
-      // Check if user was redirected from the login screen
-      // If redirected, authResult is set to the login result
-      // Else it will be null
-      initialized = true;
-
-      if (authResult) {
-        if (authResult.error) {
-          return commonModals.alert(authResult.errorDescription)
-        } else {
-          store.set('sessionToken', authResult.idToken)
-          $cookies.put('sessionToken', authResult.idToken)
-          auth.getProfile().then((profile) => {
-            store.set('profile', profile);
-          })
-        }
-
-        if (authResult.state) {
-          window.location.hash = authResult.state
-          return Promise.reject(true) // true to preventLogin
-        }
-      }
-    })
-  }
-
-  /**
-   * Check the storage token, refreshes the token if necessary
-   *
-   * The output of this is passed to pauseStateChange.
-   * Therefore if no pausing is required (i.e. auth looks ok) then
-   * return a falsy value.
-   *
-   * If pausing is required, return a Promise that resolves when auth is complete.
-   */
-  function checkStorageToken() {
-    const idToken = store.get('sessionToken')
-
-    if (idToken) {
-      if (!jwtHelper.isTokenExpired(idToken)) {
-        if (!auth.isAuthenticated) {
-          auth.authenticate({ idToken });
-          auth.getProfile().then((profile) => {
-            store.set('profile', profile);
-          })
-        }
-        return null
-      } else {
-        return auth.refreshToken()
-          .then(storeSessionToken)
-      }
-    } else {
-      return Promise.reject(null)
-    }
-  }
-})
-
-function configureGoogleMaps(uiGmapGoogleMapApiProvider) {
-  uiGmapGoogleMapApiProvider.configure({
-    key: 'AIzaSyBkFH42PlbFrsfdAnjw37qMLAxjhkMT-54',
-    libraries: 'geometry',
   })
-}
+  Vue.use(AsyncComputed)
+  Vue.use(VueRouter)
 
-function configureUrlWhitelist($sceDelegateProvider) {
-  $sceDelegateProvider.resourceUrlWhitelist([
-    'self',
-    process.env.BACKEND_URL + '/**'
-  ])
-}
+  new Vue({
+    el: '#app',
+    store: createViewStore(),
+    router: createRouter(),
+    render (h) {
+      return h(App)
+    }
+  })
+})
--- beeline-admin/router.js
+++ beeline-admin/router.js
@@ -1,248 +1,177 @@
+import _ from 'lodash'
+import VueRouter from 'vue-router'
+import {authInitializationPromise} from '@/components/AuthHelper'
 
-angular.module('beeline-admin')
-.config(function ($stateProvider, $urlRouterProvider, authProvider, $locationProvider) {
-  $locationProvider.hashPrefix('')
+import AdminsPage from '@/pages/admins.vue'
+import AssetsPage from '@/pages/assets.vue'
+import BookingsPage from '@/pages/bookings.vue'
+import CompaniesPage from '@/pages/companies.vue'
+import ContactListPage from '@/pages/contact-list.vue'
+import ContactListsPage from '@/pages/contact-lists.vue'
+import CrowdstartSummaryPage from '@/pages/crowdstart-summary.vue'
+import RoutesPage from '@/pages/routes.vue'
+import RoutePassesPage from '@/pages/route-passes.vue'
+import RoutePage from '@/pages/route.vue'
+import PromotionPage from '@/pages/promotion.vue'
+import PromotionsPage from '@/pages/promotions.vue'
+import TransactionsPage from '@/pages/transactions.vue'
+import UserPage from '@/pages/users.vue'
+import NotificationsPage from '@/pages/notifications.vue'
+import ExtendRoutesPage from '@/pages/extend-routes.vue'
+import DriversPage from '@/pages/drivers.vue'
+import SummaryPage from '@/pages/ridership-summary.vue'
+import RouteTimelinessPage from '@/pages/route-timeliness.vue'
 
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-  .state('c', {
-    url: '/c/:companyId',
-    template: '<ui-view></ui-view>',
-    controller(AdminService, $stateParams) {
-      AdminService.actingCompany = $stateParams.companyId ?
-        parseInt($stateParams.companyId) : null;
-    },
-    resolve: {
-      companyId($stateParams) {
-        return $stateParams.companyId ?
-          parseInt($stateParams.companyId) : null;
-      }
-    }
-  })
-
-  .state('c.transactions', {
-    url: '/transactions?transactionId&ticketId',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'TransactionsPage',
-    },
-  })
-
-  .state('c.routes', {
-    url: '/routes',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'RoutesPage',
-    }
-  })
-
-  .state('c.extendRoutes', {
-    url: '/extend_routes',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'ExtendRoutesPage',
-    }
-  })
-
-  .state('c.companies', {
-    url: '/companies',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'CompaniesPage',
-    }
-  })
-  .state('assets', {
-    url: '/assets',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'AssetsPage',
-    }
-  })
-  .state('c.admins', {
-    url: '/admins',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'AdminsPage',
-    }
-  })
-
-  .state('c.trips', {
-    url: '/trips/{routeId:int}/{action}',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'RoutePage',
-    }
-  })
-
-  .state('c.summary', {
-    url: '/summary',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'SummaryPage',
-    }
-  })
-
-  .state('c.bookings', {
-    url: '/bookings?routeId&tripId&userId',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'BookingsPage',
-    }
-  })
-
-  .state('c.driver', {
-    url: '/drivers',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'DriversPage',
-    }
-  })
-
-  .state('c.crowdstart-summary', {
-    url: '/crowdstart-summary',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'CrowdstartSummaryPage',
-    }
-  })
-  .state('c.test', {
-    url: '/test',
-    templateUrl: 'templates/test.html',
-    controller: function ($scope) {
-    },
-    data: {
-      requiresLogin: true,
-    }
-  })
-
-  .state('c.promotions', {
-    url: '/promotions/{promoId}',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'PromotionPage',
-    }
-  })
-
-  .state('c.promotions-list', {
-    url: '/promotions',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'PromotionsPage'
-    }
-  })
-
-  .state('c.contact-lists', {
-    url: '/contactLists',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'ContactListsPage',
-    }
-  })
-
-  .state('c.contact-list', {
-    url: '/contactLists/{contactListId}',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'ContactListPage',
-    }
-  })
-
-  .state('c.notifications', {
-    url: '/notifications',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'NotificationsPage'
-    }
-  })
-
-  .state('c.users', {
-    url: '/users/{userId}',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'UserPage'
-    }
-  })
-
-  .state('c.route-passes', {
-    url: '/route-passes?userId',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'RoutePassesPage',
-    }
-  })
-
-  .state('c.route-timeliness', {
-    url: '/route-timeliness',
-    templateUrl: 'templates/generic-vue-page.html',
-    controller: 'genericVuePageController',
-    data: {
-      requiresLogin: true,
-      page: 'RouteTimelinessPage',
-    }
-  })
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise(function ($injector, $location) {
-    const auth = $injector.get('auth')
-
-    function redirect() {
-      window.location.hash = '#/c//bookings'
-    }
-
-    // We should not do anything if auth has not been initialized,
-    // because the hash has not been parsed
-    if (auth.initialized) {
-      return '/c//bookings'
-    } else {
-      auth.authResultPromise.then(() => {
-        if (!auth.redirectRequired) { // No redirect required **by the authenticator**
-          redirect()
-        }
-        /* If redirect is required, it will be handled
-            by the code in $rootScope.$on('$stateChangeStart')
-            so we're done
-        */
+export default function () {
+  function addQueryProps(...queryProps) {
+    return (r) => {
+      return ({
+        ..._.pick(r.query, queryProps),
+        ...r.params,
+        companyId: (parseInt(r.params.companyId))
+          ? r.params.companyId
+          : null
       })
     }
-  });
-})
+  }
+
+  const routes = [
+    {
+      path: `/c/:companyId/transactions`,
+      component: TransactionsPage,
+      name: 'TransactionsPage',
+      props: addQueryProps('transactionId', 'ticketId')
+    },
+    {
+      path: `/c/:companyId/routes`,
+      component: RoutesPage,
+      name: 'RoutesPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/extend-routes`,
+      component: ExtendRoutesPage,
+      name: 'ExtendRoutesPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/companies`,
+      component: CompaniesPage,
+      name: 'CompaniesPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/assets`,
+      component: AssetsPage,
+      name: 'AssetsPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/admins`,
+      component: AdminsPage,
+      name: 'AdminsPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/trips/:routeId/:action`,
+      component: RoutePage,
+      name: 'RoutePage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/summary`,
+      component: SummaryPage,
+      name: 'SummaryPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/bookings`,
+      component: BookingsPage,
+      name: 'BookingsPage',
+      props: addQueryProps('routeId', 'tripId', 'userId'),
+    },
+    {
+      path: `/c/:companyId/drivers`,
+      component: DriversPage,
+      name: 'DriversPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/crowdstart-summary`,
+      component: CrowdstartSummaryPage,
+      name: 'CrowdstartSummaryPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/promotions`,
+      component: PromotionsPage,
+      name: 'PromotionsPage',
+      props: addQueryProps()
+    },
+    {
+      path: `/c/:companyId/promotions/:promoId`,
+      component: PromotionPage,
+      name: 'PromotionPage',
+      props: addQueryProps()
+    },
+    {
+      path: '/c/:companyId/contactLists',
+      component: ContactListsPage,
+      name: 'ContactListsPage',
+      props: addQueryProps()
+    },
+    {
+      path: '/c/:companyId/contactLists/:contactListId',
+      component: ContactListPage,
+      name: 'ContactListPage',
+      props: addQueryProps()
+    },
+    {
+      path: '/c/:companyId/notifications',
+      component: NotificationsPage,
+      name: 'NotificationsPage',
+      props: addQueryProps(),
+    },
+    {
+      path: '/c/:companyId/users',
+      component: UserPage,
+      name: 'UsersPage',
+      props: addQueryProps()
+    },
+    {
+      path: '/c/:companyId/users/:userId',
+      component: UserPage,
+      name: 'UserPage',
+      props: addQueryProps()
+    },
+    {
+      path: '/c/:companyId/route-passes',
+      component: RoutePassesPage,
+      name: 'RoutePassesPage',
+      props: addQueryProps('userId'),
+    },
+    {
+      path: '/c/:companyId/route-timeliness',
+      component: RouteTimelinessPage,
+      name: 'RouteTimelinessPage',
+      props: addQueryProps()
+    },
+    {
+      path: '*',
+      component: BookingsPage
+    }
+  ]
+  
+  const router = new VueRouter({
+    saveScrollPosition: true,
+    routes
+  })
+
+  // Force the params to be parsed first before
+  // navigation can happen
+  router.beforeEach((to, from, next) => {
+    authInitializationPromise.then(next)
+  })
+
+  return router
+}
